6e2529640171d82f17a401f6711575c0f8e93062
TeresaFlores <teresaflores1730@gmail.com>
2020-07-10 08:16:15 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PasswordRequestComponent } from './passwordRequest/password-request/password-request.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PasswordRequestComponent,
+
+
+
   ],
   imports: [
     BrowserModule
